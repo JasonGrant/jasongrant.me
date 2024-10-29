ccd16bf2d17018601e1c820c5ef7ce9eac1ec973
Jason Grant <contactjasongrant@gmail.com>
2024-10-29 18:52:25 -0400

    Added mentor profile link
    Allows users to navigate directly to ADPList and see profile. https://adplist.org/mentors/jason-grant

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f3731c9a9e07c127"; }
+/******/ 		__webpack_require__.h = function() { return "9a936764465f4805"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- app/mentoring/page.tsx
+++ app/mentoring/page.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Container, DataList, Flex, Heading } from '@radix-ui/themes';
+import { Container, Flex, Heading, Link } from '@radix-ui/themes';
 import styles from "./page.module.css";
 
 const MentoringPage: React.FC = () => {
     return (
         <Container size="4" className={styles.pageContainer}>
             <div className={styles.pageLayout}>
-                <Flex direction="column" gap="4" pt="4" className={styles.informationZone}>
-                    {/* <Heading size="5">Jason Grant</Heading> */}
+                <Flex direction="column" gap="4" pt="5" className={styles.informationZone}>
+                    <Heading size="5">Jason Grant</Heading>
+                    <Link href="https://adplist.org/mentors/jason-grant" target="_blank" rel="noopener noreferrer">View profile</Link>
                     <iframe src="https://adplist.org/widgets/impact?src=jason-grant" title="Impact" width="100%" height="100%" loading="lazy" style={{ border: '0px' }}></iframe>
                 </Flex>
                 <Flex direction="column" gap="4" className={styles.reviewsZone}>
@@ -20,16 +21,6 @@ const MentoringPage: React.FC = () => {
                         style={{ border: '0px' }}
                     ></iframe>
                 </Flex>
-                {/* <section style={{ padding: '16px', height: '100%', overflow: 'hidden', width: '100%', maxWidth: '650px' }}>
-                    <iframe
-                        src="https://adplist.org/widgets/reviews?src=jason-grant"
-                        title="All Reviews"
-                        width="100%"
-                        height="100%"
-                        loading="lazy"
-                        style={{ border: '0px' }}
-                    ></iframe>
-                </section> */}
             </div>
         </Container>
     );
